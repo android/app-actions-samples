b3671f31872c2fb9214ef2c2a223f7e783fc2b30
Carlo Huamán <cr.htorres@gmail.com>
2023-01-26 02:40:38 -0500

    feat(fitness-complete): update/migrate dependencies to AS Electric Eel  - Update ASG Plugin to 7.4  - Update Gradle plugin to 7.5  - Update Kotlin to 1.7  - Update JDK to 11  - Update targetSDK to 33  - Update minSDK to 23  - Remove Kotlin Synthetics  - Add support to ViewBinding  - Add flag to FitTrackService IMMUTABLE  - Add runtime Notification permission POST_NOTIFICATIONS

--- fitness-biis/complete/app/src/main/java/com/devrel/android/fitactions/home/FitStatsFragment.kt
+++ fitness-biis/complete/app/src/main/java/com/devrel/android/fitactions/home/FitStatsFragment.kt
@@ -24,8 +24,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.devrel.android.fitactions.R
+import com.devrel.android.fitactions.databinding.FitStatsFragmentBinding
 import com.devrel.android.fitactions.model.FitRepository
-import kotlinx.android.synthetic.main.fit_stats_fragment.*
 import java.util.concurrent.TimeUnit
 
 /**
@@ -35,41 +35,45 @@ import java.util.concurrent.TimeUnit
  */
 class FitStatsFragment : Fragment() {
 
+    private lateinit var _binding: FitStatsFragmentBinding
+    private val binding get() = _binding
+
     lateinit var actionsCallback: FitStatsActions
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fit_stats_fragment, container, false)
+    ): View {
+        _binding = FitStatsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val adapter = FitStatsAdapter()
-        statsList.adapter = adapter
+        binding.statsList.adapter = adapter
 
         val repository = FitRepository.getInstance(requireContext())
         repository.getStats().observe(viewLifecycleOwner, Observer { fitStats ->
-            statsActivityCount.text = getString(
+            binding.statsActivityCount.text = getString(
                 R.string.stats_total_count,
                 fitStats.totalCount
             )
-            statsDistanceCount.text = getString(
+            binding.statsDistanceCount.text = getString(
                 R.string.stats_total_distance,
                 fitStats.totalDistanceMeters.toInt()
             )
             val durationInMin = TimeUnit.MILLISECONDS.toMinutes(fitStats.totalDurationMs)
-            statsDurationCount.text = getString(R.string.stats_total_duration, durationInMin)
+            binding.statsDurationCount.text = getString(R.string.stats_total_duration, durationInMin)
         })
 
         repository.getLastActivities(10).observe(viewLifecycleOwner, Observer {
             adapter.submitList(it)
-            statsList.smoothScrollToPosition(0)
+            binding.statsList.smoothScrollToPosition(0)
         })
 
-        statsStartButton.setOnClickListener {
+        binding.statsStartButton.setOnClickListener {
             actionsCallback.onStartActivity()
         }
     }
